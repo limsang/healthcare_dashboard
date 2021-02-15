6f88e4040e05e387ff07cd47d3fe54064d034c01
sanghyuklim <lsh9225@naver.com>
2021-02-15 23:26:52 +0900

    init
    Former-commit-id: d0f5432cbb9c8e4ff0148e0698df3c9914b34ba3

---
+++ apple_health_data2elastic.py
@@ -0,0 +1,176 @@
+from datetime import date, datetime, timedelta as td
+import pytz
+import numpy as np
+import pandas as pd
+
+import json
+
+from elasticsearch import Elasticsearch
+from elasticsearch import helpers
+
+from es_pandas import es_pandas
+
+# instantiate elastic search
+es = Elasticsearch('[엘라스틱_서버_IP_주소]:9200')
+
+# functions to convert UTC to Shanghai time zone and extract date/time elements
+convert_tz = lambda x: x.to_pydatetime().replace(tzinfo=pytz.utc).astimezone(pytz.timezone('America/Los_Angeles'))
+get_year = lambda x: convert_tz(x).year
+get_month = lambda x: '{}-{:02}'.format(convert_tz(x).year, convert_tz(x).month) #inefficient
+get_date = lambda x: '{}-{:02}-{:02}'.format(convert_tz(x).year, convert_tz(x).month, convert_tz(x).day) #inefficient
+get_day = lambda x: convert_tz(x).day
+get_hour = lambda x: convert_tz(x).hour
+get_minute = lambda x: convert_tz(x).minute
+get_day_of_week = lambda x: convert_tz(x).weekday()
+
+
+steps = pd.read_csv("data/StepCount.csv")
+# parse out date and time elements as local time
+steps['startDate'] = pd.to_datetime(steps['startDate'])
+# parse to unix seconds since epoch
+steps['timestamp'] = pd.to_datetime(steps['startDate']).astype(int) / 10**9
+
+steps['dow'] = steps['startDate'].map(get_day_of_week)
+steps['year'] = steps['startDate'].map(get_year)
+steps['month'] = steps['startDate'].map(get_month)
+steps['date'] = steps['startDate'].map(get_date)
+steps['day'] = steps['startDate'].map(get_day)
+steps['hour'] = steps['startDate'].map(get_hour)
+steps['dow'] = steps['startDate'].map(get_day_of_week)
+dayOfWeek={0:'Monday', 1:'Tuesday', 2:'Wednesday', 3:'Thursday', 4:'Friday', 5:'Saturday', 6:'Sunday'}
+steps['weekday'] = steps['startDate'].dt.dayofweek.map(dayOfWeek)
+steps['indexId'] = (steps.index + 100).astype(str)
+
+steps = steps.fillna(value='')
+
+INDEX = 'steps'
+TYPE = 'record'
+
+# Delete if already exists
+if es.indices.exists(INDEX):
+ es.indices.delete(INDEX)
+
+# Create index
+es.indices.create(INDEX)
+
+# Add mapping
+with open('apple_health_elastic_mapping.json') as json_mapping:
+ d = json.load(json_mapping)
+
+# Create Customized Index Mappings
+es.indices.put_mapping(index=INDEX, doc_type=TYPE, body=d, include_type_name=True)
+
+
+
+es_host = 'localhost:9200'
+index = 'steps'
+
+# crete es_pandas instance
+ep = es_pandas(es_host)
+
+ep.init_es_tmpl(steps, TYPE)
+
+# Example of write data to es, use the template you create
+ep.to_es(steps, index, doc_type=TYPE)
+
+
+resting = pd.read_csv("data/RestingHeartRate.csv")
+len(resting)
+# parse out date and time elements as local time
+resting['startDate'] = pd.to_datetime(resting['startDate'])
+
+# parse to unix seconds since epoch
+resting['timestamp'] = pd.to_datetime(resting['startDate']).astype(int) / 10**9
+
+resting['dow'] = resting['startDate'].map(get_day_of_week)
+resting['year'] = resting['startDate'].map(get_year)
+resting['month'] = resting['startDate'].map(get_month)
+resting['date'] = resting['startDate'].map(get_date)
+resting['day'] = resting['startDate'].map(get_day)
+resting['hour'] = resting['startDate'].map(get_hour)
+resting['dow'] = resting['startDate'].map(get_day_of_week)
+dayOfWeek={0:'Monday', 1:'Tuesday', 2:'Wednesday', 3:'Thursday', 4:'Friday', 5:'Saturday', 6:'Sunday'}
+resting['weekday'] = resting['startDate'].dt.dayofweek.map(dayOfWeek)
+
+resting['indexId'] = (resting.index + 100).astype(str)
+
+resting = resting.fillna(value='')
+
+INDEX = 'resting_hr'
+TYPE = 'record'
+
+# Delete if already exists
+if es.indices.exists(INDEX):
+ es.indices.delete(INDEX)
+
+# Create index
+es.indices.create(INDEX)
+
+# Add mapping
+with open('apple_health_elastic_mapping.json') as json_mapping:
+ d = json.load(json_mapping)
+
+# Create Customized Index Mappings
+es.indices.put_mapping(index=INDEX, doc_type=TYPE, body=d, include_type_name=True)
+
+index = 'resting'
+
+# crete es_pandas instance
+# ep = es_pandas(es_host)
+
+ep.init_es_tmpl(resting, TYPE)
+
+# Example of write data to es, use the template you create
+ep.to_es(resting, index, doc_type=TYPE)
+
+
+hr = pd.read_csv("data/HeartRate.csv")
+len(hr)
+
+# parse out date and time elements as local time
+hr['startDate'] = pd.to_datetime(hr['startDate'])
+
+# parse to unix seconds since epoch
+hr['timestamp'] = pd.to_datetime(hr['startDate']).astype(int) / 10**9
+
+hr['dow'] = hr['startDate'].map(get_day_of_week)
+hr['year'] = hr['startDate'].map(get_year)
+hr['month'] = hr['startDate'].map(get_month)
+hr['date'] = hr['startDate'].map(get_date)
+hr['day'] = hr['startDate'].map(get_day)
+hr['hour'] = hr['startDate'].map(get_hour)
+hr['dow'] = hr['startDate'].map(get_day_of_week)
+
+dayOfWeek={0:'Monday', 1:'Tuesday', 2:'Wednesday', 3:'Thursday', 4:'Friday', 5:'Saturday', 6:'Sunday'}
+hr['weekday'] = hr['startDate'].dt.dayofweek.map(dayOfWeek)
+
+hr['indexId'] = (hr.index + 100).astype(str)
+
+hr = hr.fillna(value='')
+
+INDEX = 'hr'
+TYPE = 'record'
+
+# Delete if already exists
+if es.indices.exists(INDEX):
+ es.indices.delete(INDEX)
+
+# Create index
+es.indices.create(INDEX)
+
+# Add mapping
+with open('apple_health_elastic_mapping.json') as json_mapping:
+ d = json.load(json_mapping)
+
+# Create Customized Index Mappings
+es.indices.put_mapping(index=INDEX, doc_type=TYPE, body=d, include_type_name=True)
+
+index = 'resting'
+
+# crete es_pandas instance
+# ep = es_pandas(es_host)
+
+ep.init_es_tmpl(hr, TYPE)
+
+# Example of write data to es, use the template you create
+ep.to_es(hr, index, doc_type=TYPE)
