5ea85489493da72772bd177cc9224b05fa715978
sanghyuklim <lsh9225@naver.com>
2021-03-12 23:50:13 +0900

    반복 소스 함수로 단축
    Former-commit-id: 370d225fa881f9d5273de47653d67b544384b3f4

--- elasticsearch/apple_health_data2elastic.py
+++ elasticsearch/apple_health_data2elastic.py
@@ -6,6 +6,7 @@ import json
 from elasticsearch import Elasticsearch
 from es_pandas import es_pandas
 import sys
+import json
 
 # functions to convert UTC to Shanghai time zone and extract date/time elements
 convert_tz = lambda x: x.to_pydatetime().replace(tzinfo=pytz.utc).astimezone(pytz.timezone('America/Los_Angeles'))
@@ -17,18 +18,23 @@ get_hour = lambda x: convert_tz(x).hour
 get_minute = lambda x: convert_tz(x).minute
 get_day_of_week = lambda x: convert_tz(x).weekday()
 
+
 class HealthDataExtractor(object):
-
  def __init__(self):
+  with open('config.json', 'r') as f:
+    self.config = json.load(f)
 
-  self.ES_PORT = '9200'
-  self.IP = '192.168.0.7'
-
+  self.ES_PORT = self.config["ES_PORT"]
+  self.IP = self.config["ES_IP"]
   self.es = Elasticsearch(f"{self.IP}:{self.ES_PORT}", verify_certs=True)
-  if not self.es.ping():
-   raise ValueError("Connection failed")
 
-  self.TYPE = 'record'
+  if not self.es.ping():
+    raise ValueError("Connection failed")
+
+  else:
+   print("elasticsearch connection... ping result....", self.es.ping())
+
+  self.TYPE = self.config["TYPE"]
   self.ep = es_pandas(f"{self.IP}:{self.ES_PORT}")
 
  def check_and_generate_index(self, INDEX):
@@ -37,25 +43,31 @@ class HealthDataExtractor(object):
   # Create index
   self.es.indices.create(INDEX)
 
+ def create_dafareame_with_initial_columns(self, csv_path):
+
+  df = pd.read_csv(csv_path)
+
+  # parse out date and time elements as local time
+  df['startDate'] = pd.to_datetime(df['startDate'])
+
+  # parse to unix seconds since epoch
+  df['timestamp'] = pd.to_datetime(df['startDate']).astype(int) / 10 ** 9
+  df['dow'] = df['startDate'].map(get_day_of_week)
+  df['year'] = df['startDate'].map(get_year)
+  df['month'] = df['startDate'].map(get_month)
+  df['date'] = df['startDate'].map(get_date)
+  df['day'] = df['startDate'].map(get_day)
+  df['hour'] = df['startDate'].map(get_hour)
+  df['dow'] = df['startDate'].map(get_day_of_week)
+  dayOfWeek = {0: 'Monday', 1: 'Tuesday', 2: 'Wednesday', 3: 'Thursday', 4: 'Friday', 5: 'Saturday', 6: 'Sunday'}
+  df['weekday'] = df['startDate'].dt.dayofweek.map(dayOfWeek)
+  df['indexId'] = (df.index + 100).astype(str)
+  df = df.fillna(value='')
+  return df
+
  def gen_step_index(self):
 
-  steps = pd.read_csv("data/StepCount.csv")
-  # parse out date and time elements as local time
-  steps['startDate'] = pd.to_datetime(steps['startDate'])
-  # parse to unix seconds since epoch
-  steps['timestamp'] = pd.to_datetime(steps['startDate']).astype(int) / 10**9
-  steps['dow'] = steps['startDate'].map(get_day_of_week)
-  steps['year'] = steps['startDate'].map(get_year)
-  steps['month'] = steps['startDate'].map(get_month)
-  steps['date'] = steps['startDate'].map(get_date)
-  steps['day'] = steps['startDate'].map(get_day)
-  steps['hour'] = steps['startDate'].map(get_hour)
-  steps['dow'] = steps['startDate'].map(get_day_of_week)
-  dayOfWeek={0:'Monday', 1:'Tuesday', 2:'Wednesday', 3:'Thursday', 4:'Friday', 5:'Saturday', 6:'Sunday'}
-  steps['weekday'] = steps['startDate'].dt.dayofweek.map(dayOfWeek)
-  steps['indexId'] = (steps.index + 100).astype(str)
-  steps = steps.fillna(value='')
-
+  steps = self.create_dafareame_with_initial_columns("data/StepCount.csv")
   INDEX = 'steps'
   self.check_and_generate_index(INDEX)
 
@@ -70,26 +82,8 @@ class HealthDataExtractor(object):
   self.ep.to_es(steps, INDEX, doc_type=self.TYPE)
 
  def gen_resting_index(self):
-  resting = pd.read_csv("data/RestingHeartRate.csv")
 
-  # parse out date and time elements as local time
-  resting['startDate'] = pd.to_datetime(resting['startDate'])
-
-  # parse to unix seconds since epoch
-  resting['timestamp'] = pd.to_datetime(resting['startDate']).astype(int) / 10**9
-  resting['dow'] = resting['startDate'].map(get_day_of_week)
-  resting['year'] = resting['startDate'].map(get_year)
-  resting['month'] = resting['startDate'].map(get_month)
-  resting['date'] = resting['startDate'].map(get_date)
-  resting['day'] = resting['startDate'].map(get_day)
-  resting['hour'] = resting['startDate'].map(get_hour)
-  resting['dow'] = resting['startDate'].map(get_day_of_week)
-  dayOfWeek={0:'Monday', 1:'Tuesday', 2:'Wednesday', 3:'Thursday', 4:'Friday', 5:'Saturday', 6:'Sunday'}
-  resting['weekday'] = resting['startDate'].dt.dayofweek.map(dayOfWeek)
-
-  resting['indexId'] = (resting.index + 100).astype(str)
-
-  resting = resting.fillna(value='')
+  resting = self.create_dafareame_with_initial_columns("data/RestingHeartRate.csv")
 
   INDEX = 'resting_hr'
   self.check_and_generate_index(INDEX)
@@ -105,29 +99,8 @@ class HealthDataExtractor(object):
   self.ep.to_es(resting, INDEX, doc_type=self.TYPE)
 
  def gen_heartRate_index(self):
-  hr = pd.read_csv("data/HeartRate.csv")
 
-  # parse out date and time elements as local time
-  hr['startDate'] = pd.to_datetime(hr['startDate'])
-
-  # parse to unix seconds since epoch
-  hr['timestamp'] = pd.to_datetime(hr['startDate']).astype(int) / 10**9
-
-  hr['dow'] = hr['startDate'].map(get_day_of_week)
-  hr['year'] = hr['startDate'].map(get_year)
-  hr['month'] = hr['startDate'].map(get_month)
-  hr['date'] = hr['startDate'].map(get_date)
-  hr['day'] = hr['startDate'].map(get_day)
-  hr['hour'] = hr['startDate'].map(get_hour)
-  hr['dow'] = hr['startDate'].map(get_day_of_week)
-
-  dayOfWeek={0:'Monday', 1:'Tuesday', 2:'Wednesday', 3:'Thursday', 4:'Friday', 5:'Saturday', 6:'Sunday'}
-  hr['weekday'] = hr['startDate'].dt.dayofweek.map(dayOfWeek)
-
-  hr['indexId'] = (hr.index + 100).astype(str)
-
-  hr = hr.fillna(value='')
-
+  hr = self.create_dafareame_with_initial_columns("data/HeartRate.csv")
   INDEX = 'hr'
 
   self.check_and_generate_index(INDEX)
@@ -144,36 +117,26 @@ class HealthDataExtractor(object):
   self.ep.to_es(hr, INDEX, doc_type=self.TYPE)
 
  def gen_workOut_index(self):
-  Workout = pd.read_csv("data/Workout.csv")
+  """
+  workout csv를 통해 여러 타입의 운동관련 인덱스를 생성한다.
+  :return: 유 무산소 운동기록에 대한 인덱스
+  """
+  Workout = self.create_dafareame_with_initial_columns("data/Workout.csv")
+  cats = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
-  # parse out date and time elements as seoul time
-  Workout['startDate'] = pd.to_datetime(Workout['startDate'])
-  Workout['year'] = Workout['startDate'].map(get_year)
-  Workout['month'] = Workout['startDate'].map(get_month)
-  Workout['date'] = Workout['startDate'].map(get_date)
-  Workout['day'] = Workout['startDate'].map(get_day)
-  Workout['hour'] = Workout['startDate'].map(get_hour)
-  Workout['dow'] = Workout['startDate'].map(get_day_of_week)
-
-  dayOfWeek={0:'Monday', 1:'Tuesday', 2:'Wednesday', 3:'Thursday', 4:'Friday', 5:'Saturday', 6:'Sunday'}
-  Workout['weekday'] = Workout['startDate'].dt.dayofweek.map(dayOfWeek)
-  Workout['indexId'] = (Workout.index + 100).astype(str)
-  Workout = Workout.fillna(value='')
-
-  cardio_mask=Workout['totalDistance']>0
-  weight_trainig_mask=Workout['totalDistance'] == 0
-
-  # 체력운동과 근력을 분리
+  #유산소 운동
+  cardio_mask=Workout['totalDistance'] > 0
   CardioWorkout = Workout[cardio_mask]
-  weight_training= Workout[weight_trainig_mask]
-  # 운동강도 추가
-  weight_training['ExerciseIntensity'] = round(weight_training['totalEnergyBurned'] / weight_training['duration'])
   CardioWorkout['ExerciseIntensity'] = round(CardioWorkout['totalEnergyBurned'] / CardioWorkout['duration'])
   CardioWorkout['date'] = pd.to_datetime(CardioWorkout['date'])
   CardioWorkout = CardioWorkout[['date', 'weekday', 'duration', 'totalEnergyBurned', 'ExerciseIntensity']]
   CardioWorkout = CardioWorkout.set_index('date')
 
-  cats = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+  #무산소 운동
+  weight_trainig_mask=Workout['totalDistance'] == 0
+  weight_training= Workout[weight_trainig_mask]
+  # 운동강도 추가
+  weight_training['ExerciseIntensity'] = round(weight_training['totalEnergyBurned'] / weight_training['duration'])
 
   gymTraining = weight_training
   gymTraining['date'] = pd.to_datetime(weight_training['date'])
@@ -185,6 +148,7 @@ class HealthDataExtractor(object):
   gymTrainingPerWeekday = gymTrainingPerWeekday.mean().reindex(cats)
   # 인덱스를 일반 컬럼으로 이동
   gymTrainingPerWeekday = gymTrainingPerWeekday.reset_index()
+
   self.dataframe_to_es(CardioWorkout, 'cardioworkout', 'apple_health_elastic_cardio_workout_mapping.json')
   self.dataframe_to_es(gymTraining, 'weighttraining', 'apple_health_elastic_workout_mapping.json')
   self.dataframe_to_es(gymTrainingPerWeekday, 'weighttraining_week', 'apple_health_elastic_workout_week_mapping.json')
@@ -204,5 +168,5 @@ class HealthDataExtractor(object):
 
 if __name__ == '__main__':
     handler = HealthDataExtractor()
-    # handler.gen_heartRate_index()
-    handler.gen_workOut_index()
+    handler.gen_heartRate_index()
+    handler.gen_resting_index()
